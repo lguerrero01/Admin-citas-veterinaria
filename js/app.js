8ec54bad5ffb73e377b50e81e2b61ac285e58f7a
lguerrero1 <luisguerrero12387@gmail.com>
2021-04-18 22:51:44 -0400

    validation and show error

--- js/app.js
+++ js/app.js
@@ -10,6 +10,36 @@ const sintomasInput = document.querySelector('#sintomas');
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
+//clases
+
+class Citas {
+    constructor(){
+        this.Citas = []
+    }
+}
+
+class Ui {
+    imprimirAlerta(mensaje, tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center','alert', 'd-block', 'col-12');
+        //agregar clases dependiendo del ripo
+        if (tipo=== 'error') {
+            divMensaje.classList.add('alert-danger');
+        }else {
+            divMensaje.classList.add('alert-succes')
+        }
+
+        divMensaje.textContent = mensaje;
+
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+const ui = new Ui();
+const administarCitas = new Citas();
 
 evenListeners();
 function evenListeners () {
@@ -19,6 +49,8 @@ function evenListeners () {
     fechaInput.addEventListener('input', datosCita);
     horataInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 
 }
 
@@ -35,25 +67,22 @@ const  citaObj = {
 //agrega datos al objeto de la cita
 function datosCita (e) {
     citaObj[e.target.name] = e.target.value;
-    // console.log (citaObj)
+    console.log (citaObj)
 }
 
 
+//valida y agrega una nueva cita a la clase de citas 
+function nuevaCita (e){
+    e.preventDefault();
+    // Extraer la informacion del objeto de cita 
+    const { mascota, propietario, telefono, fecha, hora, sintomas} = citaObj;
+
+    if (mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '' ) {
+        
+        ui.imprimirAlerta('todos los campos son obligatorios', 'error');
+        return;
+    }
+
+}
 
 
-fetch("https://api-football-v1.p.rapidapi.com/v3/leagues", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "9cbb7dd534msh9232fabfe8b228cp156699jsnce115e2e5b97",
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
-	}
-})
-.then(response => {
-	return response.json();
-})
-.then(resultado => {
-    console.log(resultado)
-})
-.catch(err => {
-	console.error(err);
-});
