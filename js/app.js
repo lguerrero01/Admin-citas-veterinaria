9ffcdde5592bfe35f0e69413148b3257ca876153
lguerrero1 <luisguerrero12387@gmail.com>
2021-04-19 19:02:46 -0400

    show appointments

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ class Citas {
 
 class Ui {
     imprimirAlerta(mensaje, tipo){
+
         const divMensaje = document.createElement('div');
         divMensaje.classList.add('text-center','alert', 'd-block', 'col-12');
         //agregar clases dependiendo del ripo
@@ -40,10 +41,73 @@ class Ui {
         setTimeout(() => {
             divMensaje.remove();
         }, 3000);
+
+        
     }
+
+    imprimirCitas({citas}){
+        this.limpiarHTML();
+        citas.forEach(cita => {
+            const { mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+            
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita', 'p-3');
+            divCita.dataset.id = id;
+
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+            mascotaParrafo.innerHTML = `${mascota}`;
+
+            const propietarioParrafo = document.createElement('p');
+            propietarioParrafo.innerHTML = `<span class="font-weight-bolder">Propietario: </span> ${propietario}`;
+
+            const telefonoParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML = `<span class="font-weight-bolder">Teléfono: </span> ${telefono}`;
+
+            const fechaParrafo = document.createElement('p');
+            fechaParrafo.innerHTML = `<span class="font-weight-bolder">Fecha: </span> ${fecha}`;
+
+            const horaParrafo = document.createElement('p');
+            horaParrafo.innerHTML = `<span class="font-weight-bolder">Hora: </span> ${hora}`;
+
+            const sintomasParrafo = document.createElement('p');
+            sintomasParrafo.innerHTML = `<span class="font-weight-bolder">Síntomas: </span> ${sintomas}`;
+
+            // Agregar un botón de eliminar...
+            const btnEliminar = document.createElement('button');
+            btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+            // Añade un botón de editar...
+            const btnEditar = document.createElement('button');
+            btnEditar.onclick = () => cargarEdicion(cita);
+
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+            // Agregar al HTML
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar)
+            divCita.appendChild(btnEditar)
+
+            contenedorCitas.appendChild(divCita);
+
+        });
+    }
+    limpiarHTML() {
+        while(contenedorCitas.firstChild) {
+            contenedorCitas.removeChild(contenedorCitas.firstChild);
+        }
+   }
 }
 const ui = new Ui();
-const administarCitas = new Citas();
+const administrarCitas = new Citas();
 
 evenListeners();
 function evenListeners () {
@@ -90,14 +154,14 @@ function nuevaCita (e){
     citaObj.id = Date.now();
 
     //creando nueva cita
-    administarCitas.agregarCita({...citaObj});
+    administrarCitas.agregarCita({...citaObj});
 
     //reiniciar objeto y formulario
     reiniciarObj();
     formulario.reset();
 
     //mostrarHtml
-
+    ui.imprimirCitas(administrarCitas);
 }
 
 function reiniciarObj () {
